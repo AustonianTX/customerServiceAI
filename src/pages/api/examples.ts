2f4c098a6b2cb35938ee1d38228717909683e67b
Austin Johnson <austin@austinjohnson.me>
2022-12-08 21:27:34 -0600

    added webhook test

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -3,7 +3,7 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log("we testing the webhook!");
 
-  console.log(req.body);
+  console.log(req?.body);
 
   res.status(200).json({ pizza: "yummy" });
 };
